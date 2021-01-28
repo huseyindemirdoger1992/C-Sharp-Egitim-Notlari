62ecd5da6a05e265ad70d3e03c9b04364094b9a7
Hüseyin DEMİRDÖĞER <huseyindemirdoger1992@hotmail.com>
2021-01-28 05:59:05 +0300

    .

--- Switch-Case-Kullanimi/Form1.cs
+++ Switch-Case-Kullanimi/Form1.cs
@@ -26,27 +26,26 @@ namespace Switch_Case_Kullanimi
             // Eksileri = bir karşılaştırma yapılamaz ve bağlaçlar kullanılamaz (&&-||)
             int ilKodu; // Veri tipi oluşturuldu
             ilKodu = 1; // Veri tipine değer ataması yapıldı
-            for (int i = 0; i <= 81; i++)
 
-                switch (ilKodu) // işlenecek veri yapısı teslim edildi
-                {
+            switch (ilKodu) // işlenecek veri yapısı teslim edildi
+            {
 
-                    case 1: // gelen değer kontrol edilir ve eğer 1 e eşit ise işlem yapılır
-                        MessageBox.Show("Adana");
-                        break; // yapılan işlem sonrası yapıdan diğer değerler kontrol edilmeden çıkılır
+                case 1: // gelen değer kontrol edilir ve eğer 1 e eşit ise işlem yapılır
+                    MessageBox.Show("Adana");
+                    break; // yapılan işlem sonrası yapıdan diğer değerler kontrol edilmeden çıkılır
 
-                    case 2:// gelen değer kontrol edilir ve eğer 1 e eşit ise işlem yapılır
-                        Console.WriteLine("Adıyaman");
-                        break;// yapılan işlem sonrası yapıdan diğer değerler kontrol edilmeden çıkılır
+                case 2:// gelen değer kontrol edilir ve eğer 1 e eşit ise işlem yapılır
+                    Console.WriteLine("Adıyaman");
+                    break;// yapılan işlem sonrası yapıdan diğer değerler kontrol edilmeden çıkılır
 
-                    case 3:// gelen değer kontrol edilir ve eğer 1 e eşit ise işlem yapılır
-                        Console.WriteLine("Afyon");
-                        break;// yapılan işlem sonrası yapıdan diğer değerler kontrol edilmeden çıkılır
+                case 3:// gelen değer kontrol edilir ve eğer 1 e eşit ise işlem yapılır
+                    Console.WriteLine("Afyon");
+                    break;// yapılan işlem sonrası yapıdan diğer değerler kontrol edilmeden çıkılır
 
-                    default:// Gelen değeri karşılanmadı ise çalışır
-                        Console.WriteLine("Malesef bulunamadı! Lütfen değerleri kotrol edin.");
-                        break;// yapılan işlem sonrası yapıdan çıkılır
-                }
+                default:// Gelen değeri karşılanmadı ise çalışır
+                    Console.WriteLine("Malesef bulunamadı! Lütfen değerleri kotrol edin.");
+                    break;// yapılan işlem sonrası yapıdan çıkılır
+            }
         }
     }
 }
---
+++ Ternary-Operatoru-Kullanimi/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ternary_Operatoru_Kullanimi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // if veya else if gibi birden fazla koşul oluşturulamaz
+            // sadece 2 duruma göre haraket edebilir
+
+            // Örnek
+            int x = 7; // Veri tipi oluşturuldu ve değer ataması yapıldı
+            string deger = (x / 2 == 0) ? "Sayı çifttir" : "Sayı Tektir" ; // Koşul yazıldı
+            /*
+             Açıklama
+            string deger = işlem sonrası duruma göre değer atanacak yapı olmaktadır
+            ? = Soru işareti duruma göre haraket et demek
+            (x / 2 == 0) = Koşul geçerli ise 1. yapıyı çalıştıracaktır.("Sayı çifttir")
+            (x / 2 == 0) = Koşul geçerli değil ise 2. yapıyı çalıştıracaktır.("Sayı Tektir")
+             */
+            // Kısa ve kolay ezber yöntemi 
+            // string durum = () ? "" : "";
+
+        }
+    }
+}
